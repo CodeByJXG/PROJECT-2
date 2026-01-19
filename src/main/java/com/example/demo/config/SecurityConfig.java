341264d869f08c8d904cc53c0d06f62b0210f1a3
CodeByJXG <julhariemaddin46@gmail.com>
2026-01-19 18:40:56 +0800

    v3

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // frontend URL
+        configuration.setAllowedOrigins(List.of("https://library-management-system-kappa-black.vercel.app")); // frontend URL
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // if sending cookies/auth headers
